8070230871451e638d65102c64be9f1d731a9f1a
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-05-01 15:09:28 -0700

    got faux secret going

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ class App extends React.Component {
       username:'',
       password:'', 
       token:'',
-      admin: false,
-      show: true
+      show: false
     }
   }
 
@@ -23,19 +22,25 @@ class App extends React.Component {
   e.preventDefault();
     let encoded = base64.encode(`${this.state.username}:${this.state.password}`)
     await superagent.post('http://localhost:3333/signin').set('Authorization', `Basic ${encoded}`).set('Access-Control-Allow-Credentials', "true").then(result => {
-      // let rest = result
-      console.log(result.body)
     Cookies.set('auth-token', result.body.token)
     this.setState({token: result.body.token})
-    console.log(this.state.token)
   })
 }
 
 signUp = async (e) => {
-  e.preventDefault();
+  e.preventDefault(); 
   superagent.post('http://localhost:3333/signUp').send({username: this.state.username, password: this.state.password, role: 'user'}).then(result => {
     console.log("SIGN UP:", result.user)
-    this.setState({token: result.user})
+    this.setState({token: result.body.token})
+    Cookies.set('auth-token', result.body.token)
+  })
+}
+
+hitMe = (e) =>{
+  e.preventDefault();
+  superagent.get('http://localhost:3333/secret').set('Authorization', `Bearer ${this.state.token}`).then(result => {
+    console.log("SECRET: ", result)
+    this.setState({show: true})
     // Cookies.set('auth-token', result.user.token)
   })
 }
@@ -43,11 +48,13 @@ signUp = async (e) => {
 flip = () =>{
       
     return(
-      <>
-      {this.state.admin 
+      <div>
+      {
+        this.state.admin 
       ? <h1>Welcome ADMIN!!!</h1>
-      : <h1>Welcome USER!!!</h1>}
-      </>      
+      : <h1>Welcome USER!!!</h1>
+      }
+      </div>      
     )
   
 }
@@ -63,10 +70,14 @@ render(){
         </form> 
           <button type='submit' onClick={this.signIn}>Sign In</button>
           <button type='submit' onClick={this.signUp}>Sign Up</button>
+          <h1 show={this.show}>SEEEEEECREETTTTTS</h1>
+          <button type='submit' onClick={this.hitMe}>SECRETS</button>
+          
+
       
       </header>
       <body>
-            <flip show={this.show}/>
+            <flip/>
       </body>
     </div>
   );
